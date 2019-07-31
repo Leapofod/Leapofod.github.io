b113e1bc44f1900347394b51ca260098fa252f25
Leapofod <49672826+Leapofod@users.noreply.github.com>
2019-07-31 13:54:46 +0700

    Added Basic Pages
    Home Page and About page, along with CSS and JS file(s)

---
+++ index.js
@@ -0,0 +1,92 @@
+const navBars = document.getElementsByClassName('headerNavBar');
+
+
+const navItems = [
+	/*
+	{
+		name: 'foo',
+		link: 'home.html'
+	},
+	*/
+	{
+		name: 'about',
+		link: 'about.html',
+		dropdowns: []
+	},
+	{
+		name: 'projects',
+		link: 'projects.html',
+		dropdowns: [
+			{
+				name: 'games',
+				link: 'projects/games.html'
+			},
+			{
+				name: 'lessons',
+				link: 'projects/lessons.html'
+			}
+		]
+	}
+]
+
+loadNavBars();
+
+
+function loadNavBars() {
+
+	const navLinks = [];
+
+	for (const item of navItems) {
+
+		const listItem = document.createElement('li');
+		const itemLink = document.createElement('a');
+
+		itemLink.href = item.link;
+		itemLink.innerHTML = item.name.toUpperCase();
+		itemLink.classList.add('navBarItem');
+
+		listItem.appendChild(itemLink);
+
+		if (item.dropdowns.length > 0) {
+
+			const dropCont = document.createElement('div');
+			const dropdownItems = [];
+
+			for (const dropdown of item.dropdowns) {
+
+				const dropLink = document.createElement('a');
+
+				dropLink.href = dropdown.link;
+				dropLink.innerHTML = dropdown.name.toUpperCase();
+
+				dropdownItems.push(dropLink);
+			}
+
+			listItem.classList.add('dropdown');
+			dropCont.classList.add('dropdownContent');
+			dropdownItems.forEach(dropDown => dropCont.appendChild(dropDown));
+
+			listItem.appendChild(dropCont);
+		}
+		
+		navLinks.push(listItem);
+	}
+
+	// Adds Items to Navigation Bar
+	for (let i = 0; i < navBars.length; i++) {
+
+		for (const navElm of navLinks) {
+
+			// If 'this' current window's url has the current "button"'s link then it will set it to active mode.
+
+			if (window.location.href.indexOf(navElm.firstChild.href) > -1) {
+				navElm.firstChild.classList.add('active');
+			}
+
+			navBars.item(i).appendChild(navElm);
+		}
+		
+	}
+
+
+}
